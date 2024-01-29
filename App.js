ff250391c3796c145ddac305e163df93160a4ce4
Vivek Anand <vivek17212797@gmail.com>
2024-01-29 13:12:25 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js </Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import {StyleSheet,View,Text,TextInput,StatusBar,SafeAreaView} from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <TextInput style={styles.input}/>
+    </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex:1,
+    backgroundColor: "#fff",
+    paddingTop: StatusBar.currentHeight,  },
+    input:{
+      height:40,
+      margin:12,
+      padding:10,
+      borderWidth:1
+    }
 });
